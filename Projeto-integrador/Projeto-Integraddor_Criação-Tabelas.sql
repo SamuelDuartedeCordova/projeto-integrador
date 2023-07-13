739ac1cd6ea9ebe36b305f99f8c2132555a64565
pauloo <silvas.pr@gmail.com>
2023-07-13 17:09:39 -0300

    incluir tabelas

---
+++ Projeto-integrador/Projeto-Integraddor_Criação-Tabelas.sql
@@ -0,0 +1,28 @@
+CREATE TABLE marcas(
+ID SERIAL NOT NULL PRIMARY KEY,
+nome_do_fabricante VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE modelos(
+ID SERIAL NOT NULL PRIMARY KEY,
+nome VARCHAR(50) NOT NULL,
+cambio VARCHAR(20) NULL,
+combustivel VARCHAR(50) NULL,
+potencia INTEGER NULL,
+cilindrada INTEGER NULL,
+carroceria VARCHAR(50), 
+portas INTEGER NULL,
+cor VARCHAR(20) NULL,
+id_marcas integer NOT NULL,
+FOREIGN KEY (id_marcas) REFERENCES marcas(id)
+);
+
+CREATE TABLE veiculos(
+ID SERIAL NOT NULL PRIMARY KEY,
+renavam INTEGER NOT NULL UNIQUE,
+placa CHAR(10) NOT NULL UNIQUE,
+ano_fabricacao INTEGER NOT NULL,
+chassi CHAR(17) NOT NULL UNIQUE,
+id_modelos integer NOT NULL,
+FOREIGN KEY (id_modelos) REFERENCES modelos(id)
+);
